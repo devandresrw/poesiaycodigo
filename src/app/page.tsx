4f7754e116c13f9c0a5ae2ba0d9dd6b4315f745c
rw <rw@etb>
2024-09-11 12:26:22 -0500

    Responsive logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 import Image from "next/image";
-import style from '@/app/styles.module.css'
 
 export default function Home() {
   return (
     <>
-      <div className={style.container}>
+      <div className="w-dvw h-dvh flex flex-row 
+      justify-center items-center">
+        <div className="">
         <Image
           src={'/logo.svg'}
           width={500}
           height={500}
           alt="logo"
+          className="w-full h-auto 
+          max-w-xs sm:max-w-sm 
+          md:max-w-md lg:max-w-lg 
+          xl:max-w-xl"
         />
+        </div>
       </div>
     </>
   );
