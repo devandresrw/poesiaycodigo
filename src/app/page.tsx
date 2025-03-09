1b86d0b18ef9cc34897eeb44dc91fc835bcc4bd2
devandresrw <aroldan0@misena.edu.co>
2025-03-08 19:18:04 -0500

    Routes structure definitive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div className="w-dvw h-dvh flex flex-row 
-      justify-center items-center">
+      <div className="w-dvw h-dvh flex flex-col 
+      justify-center items-center gap-5">
         <div className="">
-        <Image
-          src={'/logo.svg'}
-          width={500}
-          height={500}
-          alt="logo"
-          className="w-full h-auto 
+          <Image
+            src={'/logo.svg'}
+            width={500}
+            height={500}
+            alt="logo"
+            className="w-full h-auto 
           max-w-xs sm:max-w-sm 
           md:max-w-md lg:max-w-lg 
           xl:max-w-xl"
-        />
+          />
         </div>
+
       </div>
     </>
   );
